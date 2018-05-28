668b0382e1822399640900bffb3daed6641aa371
Priya Ranjan Singh <mail@priyaranjan.net>
2018-05-28 16:46:24 +0200

    add a new page for about

---
+++ src/pages/about.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class AboutPage extends Component {
+  render() {
+    return (
+      <div className="about-container">
+        <p>About me.</p>
+      </div>
+    );
+  }
+}
+
+export default AboutPage;
